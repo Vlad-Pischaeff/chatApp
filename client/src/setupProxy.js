8846548d09dba1339c1c083dfd4a1c5513723df4
Vlad-Pischaeff <pischaeff@gmail.com>
2020-01-16 18:24:29 +0500

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
-// const port = process.env.PORT || 3001
+// // const port = process.env.PORT || 3001
 const port = 3001
 
 module.exports = function(app) {
-    app.use(proxy('/api/*', { target: `http://localhost:${port}`, ws: true }))
+    app.use(proxy('/api/*', { target: `http://localhost:${port}`, wss: true }))
 }
