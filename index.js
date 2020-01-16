0f12ff2539c256f6116a227f9c5c17d88801aa1f
Vlad-Pischaeff <pischaeff@gmail.com>
2020-01-16 15:28:52 +0500

    fixes

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 
 
 async function start() {
   try {
-     await mongoose.connect('mongodb+srv://vlad:123321@cluster0-pfbwp.mongodb.net/chatapp', options
-    //  {
-  //     useNewUrlParser: true,
-  //     useFindAndModify: false,
-  //     useUnifiedTopology: true,
-  //     socketTimeoutMS: 30000,
-  //     keepAlive: 300000
-  // }
+     await mongoose.connect('mongodb+srv://vlad:123321@cluster0-pfbwp.mongodb.net/chatapp', 
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      socketTimeoutMS: 30000,
+      keepAlive: 300000
+    }
   )
   
     server.listen(PORT, () => {
